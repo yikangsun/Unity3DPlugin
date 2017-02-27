76c24c0905b74e1aaa00364da7e833109e348445
yikangsun <sunyikang@gmail.com>
2017-02-27 15:25:40 +0800

    be able to invoke class' static functions

---
+++ UnityProject/Assets/Arithmetic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Math
+{
+	public class Arithmetic
+	{
+		[DllImport ("Arithmetic", EntryPoint = "addition", CallingConvention = CallingConvention.Cdecl)]
+		public static extern float addition (float val_1, float val_2);
+
+		[DllImport ("Arithmetic", EntryPoint = "substraction", CallingConvention = CallingConvention.Cdecl)]
+		public static extern float substraction (float val_1, float val_2);
+
+		[DllImport ("Arithmetic", EntryPoint = "multiplication", CallingConvention = CallingConvention.Cdecl)]
+		public static extern float multiplication (float val_1, float val_2);
+
+		[DllImport ("Arithmetic", EntryPoint = "division", CallingConvention = CallingConvention.Cdecl)]
+		public static extern float division (float val_1, float val_2);
+	}
+}
+
---
+++ UnityProject/Assets/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using Math;
+
+namespace App
+{
+	public class Calculator : MonoBehaviour
+	{
+		void Start ()
+		{
+			Debug.Log (Arithmetic.addition (5, 5));
+			Debug.Log (Arithmetic.substraction (10, 5));
+			Debug.Log (Arithmetic.multiplication (2, 5));
+			Debug.Log (Arithmetic.division (10, 2));
+		}
+	}
+}
+
